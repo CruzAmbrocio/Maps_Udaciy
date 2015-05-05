39adb2b878e1daa5292fb9133a546b56edfa19f9
CruzAmbrocio <cruzambrocio1409@gmail.com>
2015-05-04 18:29:38 -0600

    se ha hecho mas pruebas con los mapas faltaria estructurarlo

--- js/script.js
+++ js/script.js
@@ -1,44 +1,62 @@
 //Java code.
+function initialize(){
+  var latlng =new google.maps.LatLng(14.64072,-90.51327);
+  var myOptions = {
+    zoom:50,
+    center: latlng,
+    mapTypeId : google.maps.mapTypeId.HYBRID
+  };
+  var map = new google.maps.Map(document.getElementById('map_canvas'),myOptions);
+  var image ='../images/localization.png';
+  var myLatLng = new google.maps.LatLng(14.5973001,-90.52634119999999);
+  var beachMarker = new google.maps.Marker({
+    position: myLatLng,
+    map:map,
+    icon:image,
+    animation:google.maps.Animation.BOUNCE,
+    title:"hola que hace!!..."
+  });
+}
+
+
+
+
+
+
+
+/*
 var map;
-var infowindow;
 
 function initialize() {
-  var loc = new google.maps.LatLng(14.64072,-90.51327);
-
+  // Create a map centered in Pyrmont, Sydney (Australia).
   map = new google.maps.Map(document.getElementById('map-canvas'), {
-    center: loc,
+    center: {lat: -33.8666, lng: 151.1958},
     zoom: 15
   });
 
+  // Search for Google's office in Australia.
   var request = {
-    location: loc,
-    radius: 500,
-    types: ['store']
+    location: map.getCenter(),
+    radius: '500',
+    query: 'bar'
   };
-  infowindow = new google.maps.InfoWindow();
+
   var service = new google.maps.places.PlacesService(map);
-  service.nearbySearch(request, callback);
+  service.textSearch(request, callback);
 }
 
+// Checks that the PlacesServiceStatus is OK, and adds a marker
+// using the place ID and location from the PlacesService.
 function callback(results, status) {
   if (status == google.maps.places.PlacesServiceStatus.OK) {
-    for (var i = 0; i < results.length; i++) {
-      createMarker(results[i]);
-    }
+    var marker = new google.maps.Marker({
+      map: map,
+      place: {
+        placeId: results[0].place_id,
+        location: results[0].geometry.location
+      }
+    });
   }
 }
 
-function createMarker(place) {
-  var placeLoc = place.geometry.location;
-  var marker = new google.maps.Marker({
-    map: map,
-    position: place.geometry.location
-  });
-
-  google.maps.event.addListener(marker, 'click', function() {
-    infowindow.setContent(place.name);
-    infowindow.open(map, this);
-  });
-}
-
-google.maps.event.addDomListener(window, 'load', initialize);
+google.maps.event.addDomListener(window, 'load', initialize);*/
