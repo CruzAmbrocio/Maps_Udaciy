90897fc866aa0e56b495279b54a5a1db2f4ab9b9
CruzAmbrocio <cruzambrocio1409@gmail.com>
2015-05-05 10:52:57 -0600

    se ha hecho algunos cambios nada importante

--- js/script.js
+++ js/script.js
@@ -1,36 +1,10 @@
 //Java code.
-function initialize(){
-  var latlng =new google.maps.LatLng(14.64072,-90.51327);
-  var myOptions = {
-    zoom:50,
-    center: latlng,
-    mapTypeId : google.maps.mapTypeId.HYBRID
-  };
-  var map = new google.maps.Map(document.getElementById('map_canvas'),myOptions);
-  var image ='../images/localization.png';
-  var myLatLng = new google.maps.LatLng(14.5973001,-90.52634119999999);
-  var beachMarker = new google.maps.Marker({
-    position: myLatLng,
-    map:map,
-    icon:image,
-    animation:google.maps.Animation.BOUNCE,
-    title:"hola que hace!!..."
-  });
-}
 
-
-
-
-
-
-
-/*
 var map;
-
 function initialize() {
   // Create a map centered in Pyrmont, Sydney (Australia).
   map = new google.maps.Map(document.getElementById('map-canvas'), {
-    center: {lat: -33.8666, lng: 151.1958},
+    center: {lat: 14.64072, lng: -90.51327},
     zoom: 15
   });
 
@@ -38,7 +12,7 @@ function initialize() {
   var request = {
     location: map.getCenter(),
     radius: '500',
-    query: 'bar'
+    query: 'store'
   };
 
   var service = new google.maps.places.PlacesService(map);
@@ -59,4 +33,34 @@ function callback(results, status) {
   }
 }
 
-google.maps.event.addDomListener(window, 'load', initialize);*/
+google.maps.event.addDomListener(window, 'load', initialize);
+
+
+
+
+
+
+
+
+
+
+
+/*function initialize(){
+  var latlng =new google.maps.LatLng(-12.110306, -77.014074);
+  var myOptions = {
+    zoom:50,
+    center: latlng,
+    mapTypeId : google.maps.mapTypeId.HYBRID
+  };
+  var map = new google.maps.Map(document.getElementById('map_canvas'),myOptions);
+
+  var image ='http://www.xn--romaa-rta.pe/pelota.gif';
+  var myLatLng = new google.maps.LatLng(-12.109983, -77.014847);
+  var beachMarker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    icon: image,
+    animation: google.maps.Animation.BOUNCE,
+    title:"hola que hace!!..."
+  });
+}*/
