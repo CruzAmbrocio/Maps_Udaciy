c93bb1a452aab7661c8ed82cfef441b5eeb48553
CruzAmbrocio <cruzambrocio1409@gmail.com>
2015-05-12 17:31:55 -0600

    se agrega los botones con sus efectos tambien se hace funcionar que al hacer click se agregue un marcador, Tambien se han agregado la libreria  de todos lo iconos icomoon

--- js/app.js
+++ js/app.js
@@ -1,4 +1,18 @@
-var myApp = angular.module('myApp', ["ngMap"]);
+var app=angular.module('myApp', ['ngMap']);
+
+  app.controller('EventArgumentsCtrl', function($scope) {
+    var map;
+    $scope.$on('mapInitialized', function(evt, evtMap) {
+      map = evtMap;
+      $scope.placeMarker = function(e) {
+        var marker = new google.maps.Marker({position: e.latLng, map: map});
+        map.panTo(e.latLng);
+      }
+    });
+  });
+
+
+/*
 myApp.directive('googleplace', function() {
     return {
         require: 'ngModel',
@@ -24,4 +38,49 @@ myApp.directive('googleplace', function() {
 //myApp.factory('myService', function() {});
 function MyCtrl($scope) {
     $scope.gPlace;
-}
+}*/
+
+
+
+
+/*
+ myApp.controller('EventArgumentsCtrl', function($scope) {
+    var map;
+    $scope.$on('mapInitialized', function(evt, evtMap) {
+      map = evtMap;
+      $scope.placeMarker = function(e) {
+        var marker = new google.maps.Marker({position: e.latLng, map: map});
+        map.panTo(e.latLng);
+      }
+    });
+  });*/
+
+
+
+/*
+myApp.directive('googleplace', function() {
+    return {
+        require: 'ngModel',
+        scope: {
+            ngModel: '=',
+            details: '=',
+        },
+        link: function(scope, element, attrs, model) {
+            var options = {
+                types: [],
+                componentRestrictions: {}
+            };
+            scope.gPlace = new google.maps.places.Autocomplete(element[0], options);
+            google.maps.event.addListener(scope.gPlace, 'place_changed', function() {
+                scope.$apply(function() {
+                    scope.details = scope.gPlace.getPlace();
+                    model.$setViewValue(element.val());
+                });
+            });
+        }
+    };
+});
+//myApp.factory('myService', function() {});
+function MyCtrl($scope) {
+    $scope.gPlace;
+}*/
