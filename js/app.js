000902a34fda3a45470e8d19a477043a4173c03b
CruzAmbrocio <cruzambrocio1409@gmail.com>
2015-05-19 10:42:37 -0600

    se ha hecho pruebas y se ha allado un nuevo codigo

--- js/app.js
+++ js/app.js
@@ -16,20 +16,18 @@ var app=angular.module('myApp', ['ngMap']);
     var iterator=0;
     var numMark=0;
     var idMark="";
+    var marker="";
+    var image ="images/star.png"
 
-    var cont1= '<p class="InputSearch">hola que hace<p>';
+    $scope.addMarker = function() {
+    var cont1= '<p>hola <b>que hace</b><p>';
     var infowindow = new google.maps.InfoWindow({
         content:cont1
     });
-       google.maps.event.addListener($scope.neighborhoods[0],'click', function() {
-        infowindow.open(map,$scope.neighborhoods[0]);
-      });
-    var image ="images/star.png"
-    $scope.addMarker = function() {
       for (var i=0; i<$scope.neighborhoods.length; i++) {
         $timeout(function() {
           // add a marker this way does not sync. marker with <marker> tag
-          new google.maps.Marker({
+          var marker= new google.maps.Marker({
             position: $scope.neighborhoods[iterator++],
             map: $scope.map,
             draggable: false,
@@ -37,28 +35,81 @@ var app=angular.module('myApp', ['ngMap']);
             icon:image,
             title:idMark
           });console.log($scope.neighborhoods[numMark++])
+
           if (iterator == 4){
             idMark="four";
             console.log(idMark)
             console.log(iterator)
+
+
+
           }else if (iterator==1){
             idMark="one";
             console.log(idMark)
             console.log(iterator)
+
+       google.maps.event.addListener($scope.neighborhoods[iterator[1]],'click', function() {
+        infowindow.open(map,$scope.neighborhoods[iterator[1]]);
+      });
+
           }else if (iterator==2){
             idMark="two";
             console.log(idMark)
             console.log(iterator)
+
           }else if (iterator==3){
             idMark="three";
             console.log(idMark)
             console.log(iterator)
+
           }else{
             console.log("un error")
           };
         }, i * 300);
       }
     }
+var markers = Array();
+var infowindowActivo = false;
+function setGoogleMarkers(map, locations) {
+    // Definimos los iconos a utilizar con sus medidas
+    var icon1 = new google.maps.MarkerImage(
+        "http://www.vinx.info/uploads/editor/map-green.png",
+        new google.maps.Size(20, 30)
+    );
+    var icon2 = new google.maps.MarkerImage(
+        "http://www.vinx.info/uploads/editor/map-orange.png",
+        new google.maps.Size(20, 30)
+    );
+    var icon3 = new google.maps.MarkerImage(
+        "http://www.vinx.info/uploads/editor/map-red.png",
+        new google.maps.Size(20, 30)
+    );
+
+
+
+
+
+    for(var i=0; i<locations.length; i++) {
+        var elPunto = locations[i];
+        var myLatLng = new google.maps.LatLng(elPunto[1], elPunto[2]);
+
+        markers[i]=new google.maps.Marker({
+            position: myLatLng,
+            map: map,
+            icon: eval(elPunto[3]),
+            title: elPunto[0]
+        });
+        markers[i].infoWindow=new google.maps.InfoWindow({
+            content: elPunto[4]
+        });
+        google.maps.event.addListener(markers[i], 'click', function(){      
+            if(infowindowActivo)
+                infowindowActivo.close();
+            infowindowActivo = this.infoWindow;
+            infowindowActivo.open(map, this);
+        });
+    }
+}
   });
 /*
 myApp.directive('googleplace', function() {
