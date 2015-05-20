9b87270ad49074562e12f2b9863f55d99e6b682c
CruzAmbrocio <cruzambrocio1409@gmail.com>
2015-05-20 09:12:48 -0600

    se ha agregado los marcadores con los infowidows

--- js/app.js
+++ js/app.js
@@ -1,116 +1,65 @@
 var app=angular.module('myApp', ['ngMap']);
-  app.controller('MarkerAnimationsIterationCtrl', function($scope, $timeout) {
+  app.controller('MarkerAnimations', function($scope) {
     $scope.neighborhoods = [
-      new google.maps.LatLng(52.5088241, 13.334207800000058),
-      new google.maps.LatLng(52.549061, 13.422975),
-      new google.maps.LatLng(52.497622, 13.396110),
-      new google.maps.LatLng(52.517683, 13.394393)
+    ["Humannplatz Germany", "52.549061", "13.422975", "icon1", "<div>Humannplatz <b>Germany</b></div>"],
+    ["Humboldt University of Berlin", "52.517683", "13.394393", "icon2", "<div><b>Humboldt</b> University of Berlin</div>"],
+    ["Zoo Berlin", "52.5088241", "13.334207800000058", "icon3", "<div><b>ZOO</b>Berlin</div>"],
+    ["Brachvogelstraße","52.497622", "13.396110", "icon4", "<div><b>Brachvogelstraße</b>Berlin</div>"],
+    ["Museum für Naturkunde","52.540211","13.362722","icon5","<div><b>Museum für Naturkunde</b>Berlin</div>"],
     ];console.log($scope.neighborhoods)
-    $scope.toggleBounce = function() {
-      if (this.getAnimation() != null) {
-        this.setAnimation(null);
-      } else {
-        this.setAnimation(google.maps.Animation.BOUNCE);
-      }
-    };
-    var iterator=0;
-    var numMark=0;
-    var idMark="";
-    var marker="";
-    var image ="images/star.png"
-
-    $scope.addMarker = function() {
-    var cont1= '<p>hola <b>que hace</b><p>';
-    var infowindow = new google.maps.InfoWindow({
-        content:cont1
-    });
-      for (var i=0; i<$scope.neighborhoods.length; i++) {
-        $timeout(function() {
-          // add a marker this way does not sync. marker with <marker> tag
-          var marker= new google.maps.Marker({
-            position: $scope.neighborhoods[iterator++],
-            map: $scope.map,
-            draggable: false,
-            animation: google.maps.Animation.DROP,
-            icon:image,
-            title:idMark
-          });console.log($scope.neighborhoods[numMark++])
-
-          if (iterator == 4){
-            idMark="four";
-            console.log(idMark)
-            console.log(iterator)
-
-
-
-          }else if (iterator==1){
-            idMark="one";
-            console.log(idMark)
-            console.log(iterator)
-
-       google.maps.event.addListener($scope.neighborhoods[iterator[1]],'click', function() {
-        infowindow.open(map,$scope.neighborhoods[iterator[1]]);
-      });
-
-          }else if (iterator==2){
-            idMark="two";
-            console.log(idMark)
-            console.log(iterator)
-
-          }else if (iterator==3){
-            idMark="three";
-            console.log(idMark)
-            console.log(iterator)
-
-          }else{
-            console.log("un error")
-          };
-        }, i * 300);
-      }
+    $scope.mapa=function(){
+        var A =52.520816;
+        var F = 13.410186;
+        var mapOptions = {
+            zoom: 12,
+            center: new google.maps.LatLng(A, F),
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+        var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+        $scope.addMarker(map, $scope.neighborhoods);
     }
-var markers = Array();
-var infowindowActivo = false;
-function setGoogleMarkers(map, locations) {
-    // Definimos los iconos a utilizar con sus medidas
+    var marker =Array();
+    var infowindow=false;
+$scope.addMarker=function (map, loc) {
     var icon1 = new google.maps.MarkerImage(
-        "http://www.vinx.info/uploads/editor/map-green.png",
-        new google.maps.Size(20, 30)
+        "images/star.png"
     );
     var icon2 = new google.maps.MarkerImage(
-        "http://www.vinx.info/uploads/editor/map-orange.png",
-        new google.maps.Size(20, 30)
+        "images/star.png"
     );
     var icon3 = new google.maps.MarkerImage(
-        "http://www.vinx.info/uploads/editor/map-red.png",
-        new google.maps.Size(20, 30)
+        "images/star.png"
     );
-
-
-
-
-
-    for(var i=0; i<locations.length; i++) {
-        var elPunto = locations[i];
-        var myLatLng = new google.maps.LatLng(elPunto[1], elPunto[2]);
-
-        markers[i]=new google.maps.Marker({
+    var icon4 = new google.maps.MarkerImage(
+        "images/star.png"
+    );
+    var icon5 = new google.maps.MarkerImage(
+        "images/star.png"
+    );
+    for(var i=0; i<loc.length; i++) {
+        var locMark = loc[i];
+        var myLatLng = new google.maps.LatLng(locMark[1], locMark[2]);
+        marker[i]=new google.maps.Marker({
             position: myLatLng,
             map: map,
-            icon: eval(elPunto[3]),
-            title: elPunto[0]
+            animation: google.maps.Animation.DROP,
+            icon: eval(locMark[3]),
+            title: locMark[0]
         });
-        markers[i].infoWindow=new google.maps.InfoWindow({
-            content: elPunto[4]
+        marker[i].infoWindow=new google.maps.InfoWindow({
+            content: locMark[4]
         });
-        google.maps.event.addListener(markers[i], 'click', function(){      
-            if(infowindowActivo)
-                infowindowActivo.close();
-            infowindowActivo = this.infoWindow;
-            infowindowActivo.open(map, this);
+        google.maps.event.addListener(marker[i], 'click', function(){      
+            if(infowindow)
+                infowindow.close();
+            infowindow = this.infoWindow;
+            infowindow.open(map, this);
         });
     }
-}
+    }
   });
+
+
 /*
 myApp.directive('googleplace', function() {
     return {
--- js/app2.js
+++ js/app2.js
@@ -1,14 +1,1 @@
 
-/*  app.controller('EventArgumentsCtrl', function($scope) {
-
-    var map;
-    var image ="../images/localization.png";
-    $scope.$on('mapInitialized', function(evt, evtMap) {
-      map = evtMap;
-      $scope.placeMarker = function(e) {
-        var marker = new google.maps.Marker({position: e.latLng, map: map});
-        map.panTo(e.latLng);
-      }
-    });
-  });
-*/
