db6c31c01972d5ec109763a3ab4d8d6db074f20e
CruzAmbrocio <cruzambrocio1409@gmail.com>
2015-05-20 18:07:45 -0600

    se ha agregado imagenes e informacion de los marcadores

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 var app=angular.module('myApp', ['ngMap']);
   app.controller('MarkerAnimations', function($scope) {
     $scope.neighborhoods = [
-    ["Humannplatz Germany", "52.549061", "13.422975", "icon1", "<div>Humannplatz <b>Germany</b></div>"],
-    ["Humboldt University of Berlin", "52.517683", "13.394393", "icon2", "<div><b>Humboldt</b> University of Berlin</div>"],
-    ["Zoo Berlin", "52.5088241", "13.334207800000058", "icon3", "<div><b>ZOO</b>Berlin</div><br/><p></P>"],
-    ["Brachvogelstraße","52.497622", "13.396110", "icon4", "<div><b>Brachvogelstraße</b>Berlin</div>"],
-    ["Museum für Naturkunde","52.540211","13.362722","icon5","<div><b>Museum für Naturkunde</b>Berlin</div>"],
+    ["Humannplatz Germany", "52.54875699999999", "13.420661999999993", "icon1", "<div class='aling'>Humannplatz<b>Germany</b></div><br/><a href='http://www.yelp.com/biz/humannplatz-berlin' class='aling'>Web Site Humannplatz</a><br/><img src='http://cdn.c.photoshelter.com/img-get2/I0000P.PhuJ2D2gY/fit=1000x750/Subway-Berlin-34.jpg' class='images'></img>"],
+    ["Humboldt University of Berlin", "52.517683", "13.394393", "icon2", "<div class='aling'><b>Humboldt</b> University of Berlin</div> <br/><a href='https://www.hu-berlin.de/de'> <p class='aling'>Web site Humboldt University of Berlin</p></a> <br/><img src='https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSKtAY9wbL9c53nvRY_aoJ20rGAiRYfReu8xrnvg7YkUZDDfgjH' class='images'>"],
+    ["Zoo Berlin", "52.5088241", "13.334207800000058", "icon3", "<div class='aling'><b>ZOO</b>Berlin</div><br/><a href='http://www.zoo-berlin.de/de'><p class='aling'>Web site ZOO Berlin</p></a><img src='http://www.guiadealemania.com/wp-content/uploads/2012/05/zoo-berlin.png' class='images'></div>"],
+    ["East Side Gallery","52.504341588222424","13.439855598144504", "icon4", "<div class='aling'><b>East Side Gallery</b>Berlin</div><br/><a href='http://www.eastsidegallery-berlin.de/' class='aling'> Web Site East Side Gallery</a> <br/><img src='https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRRCZF75VTfqOEl6JPtJAjdGWtrz9KnBwCoXaPj41QF9WzSRBIq' class='images'></img>"],
+    ["Jewish Museum Berlin", "52.5023115","13.395446900000024","icon5","<div class='aling'><b>Jewish Museum</b>Berlin</div><br/><a href='http://www.jmberlin.de/main/EN/homepage-EN.php'> <p class='aling'>Web Site Jewish Museum</p></a><img src='https://bronwynvowles.files.wordpress.com/2011/10/jewish-museum-large.jpeg' class='images'></img>"],
     ];console.log($scope.neighborhoods)
     $scope.mapa=function(){
         var A =52.520816;
@@ -21,7 +21,7 @@ var app=angular.module('myApp', ['ngMap']);
     var marker =Array();
     var infowindow=false;
     var image=icon1 = new google.maps.MarkerImage("images/star.png");
-$scope.addMarker=function (map, loc) {
+    $scope.addMarker=function (map, loc) {
     var icon1 = image;
     var icon2 = image;
     var icon3 = image;
@@ -46,7 +46,7 @@ $scope.addMarker=function (map, loc) {
             infowindow = this.infoWindow;
             infowindow.open(map, this);
         });
-    }
+    }console.log($scope.neighborhoods[4])
     }
   });
 
